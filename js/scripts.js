7051de8b103ac6bf64f9d315645416c9a788f7fc
Student <example@example.com>
2019-02-11 19:00:19 -0800

    add initial files

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var addressInput = $("input#address").val();
+
+    $(".name").text(nameInput);
+    $(".address").text(addressInput);
+
+    $("#inputForm").hide();
+    $("#receipt").show();
+
+    event.preventDefault();
+  });
+});
